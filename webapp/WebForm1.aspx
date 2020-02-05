93861b4450bf8a67c887ea2fd59fff044d32cbd2
apfr <ap.francalancia@gmail.com>
2020-02-05 17:40:13 -0300

    Add project files.

---
+++ webapp/WebForm1.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="webapp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body style="height: 308px">
+    <form id="form1" runat="server">
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</th>
+                    <th style="text-align: left">COMPONENTES</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Caja de Texto</td>
+                    <td>
+                        <input type="text" id="text" runat="server" />
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>Combo</td>
+                    <td>
+                        <select id="combo" runat="server">
+                            <option>Argentina</option>
+                            <option>Italia</option>
+                            <option>Alemania</option>
+                            <option>Portugal</option>
+                        </select></td>
+                </tr>
+                <tr>
+                    <td>Imagen</td>
+                    <td>
+                        <input type="file" id="file" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>CheckBox</td>
+                    <td>
+                        <input type="checkbox" id="checkboxSI" runat="server" />SI
+                        <input type="checkbox" id="checkboxNO" runat="server" />NO
+                    </td>
+                </tr>
+                <tr>
+                    <td>Hipervínculo</td>
+                    <td><a href="http://www.google.com" target="_blank" id="web" runat="server">Google</a></td>
+                </tr>
+                <tr>
+                    <td>Radio Button</td>
+                    <td>
+                        <input type="radio" name="radio" id="alta" runat="server" />Alta
+                        <input type="radio" name="radio" id="media" runat="server" />Media
+                        <input type="radio" name="radio" id="baja" runat="server" />Baja
+                    </td>
+                </tr>
+                <tr>
+                    <td>Campo Oculto</td>
+                    <td>
+                        <input type="hidden" id="hidden" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>Botón</td>
+                    <td>
+                        <input type="submit" id="submit" value="Button" onserverclick="submit_ServerClick" runat="server" /></td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
+
+    <table id="tabla2" runat="server" visible="false" border="1">
+    </table>
+</body>
+</html>
---
+++ webapp/WebForm1.aspx.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace webapp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {            
+
+        }
+
+        protected void submit_ServerClick(object sender, EventArgs e)
+        {
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila0 = new HtmlTableRow();
+
+            fila0.Controls.Add(new HtmlTableCell
+            {
+                InnerHtml = "Datos ingresados"
+            }
+            );
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila1 = new HtmlTableRow();
+
+            fila1.Controls.Add(new HtmlTableCell
+            {
+                InnerHtml = "Caja de texto: " + text.Value
+            }
+            );
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila2 = new HtmlTableRow();
+
+            fila2.Controls.Add(new HtmlTableCell
+            {
+                InnerHtml = "Combo: " + combo.Value
+            }
+            );
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila3 = new HtmlTableRow();
+            HtmlTableCell celdaImagen = new HtmlTableCell();
+
+            celdaImagen.InnerText = "Imagen: ";
+            celdaImagen.Controls.Add(saveFile());
+
+            fila3.Controls.Add(celdaImagen);
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila4 = new HtmlTableRow();
+
+            fila4.Controls.Add(new HtmlTableCell
+            {
+                InnerHtml = "CheckBox: " + getCheckBox()
+            }
+            );
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila5 = new HtmlTableRow();
+
+            fila5.Controls.Add(new HtmlTableCell
+            {
+                InnerHtml = "Hipervínculo: " + web.HRef
+            }
+            );
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila6 = new HtmlTableRow();
+
+            fila6.Controls.Add(new HtmlTableCell
+            {
+                InnerHtml = "Radio Button: " + getRadioButton()
+            }
+            );
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRow fila7 = new HtmlTableRow();
+
+            fila7.Controls.Add(new HtmlTableCell
+            {
+                InnerHtml = "Campo Oculto: " + hidden.Value
+            }
+            );
+
+// -----------------------------------------------------------------------------
+
+            HtmlTableRowCollection filas = tabla2.Rows;
+            filas.Add(fila0);
+            filas.Add(fila1);
+            filas.Add(fila2);
+            filas.Add(fila3);
+            filas.Add(fila4);
+            filas.Add(fila5);
+            filas.Add(fila6);
+            filas.Add(fila7);
+            tabla2.Visible = true;
+        }
+
+// ------------------------ MÉTODOS --------------------------------
+
+        protected string getCheckBox()
+        {
+            string valor = "";
+            if (checkboxSI.Checked == true)
+            {
+                valor = "SI";
+            }
+            else if (checkboxNO.Checked == true)
+            {
+                valor = "NO";
+            }
+            return valor;
+        }
+
+        protected string getRadioButton()
+        {
+            string valor = "";
+            if (alta.Checked == true)
+            {
+                valor = "Alta";
+            }
+            else if (media.Checked == true)
+            {
+                valor = "Media";
+            }
+            else if (baja.Checked == true)
+            {
+                valor = "Baja";
+            }
+            return valor;
+        }
+
+
+        protected HtmlImage saveFile()
+        {
+            file.PostedFile.SaveAs("C:\\Users\\fsociety\\source\\repos\\WebFormHTML\\images\\imagen.jpg");
+            HtmlImage imagen = new HtmlImage();
+            imagen.Height = 100;
+            imagen.Width = 180;
+            imagen.Src = "images\\imagen.jpg";
+            return imagen;
+        }
+    }
+}
